2621ec4fb3a4cd62051b59000249889e1b116231
Shubham <dev4fun007@gmail.com>
2019-06-12 01:49:50 +0530

    some more scrapping - till each week

--- src/main/java/bytes/sync/App.java
+++ src/main/java/bytes/sync/App.java
@@ -1,13 +1,19 @@
 package bytes.sync;
 
+import bytes.sync.selenium.Scrapper;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    private static String courseOverviewUrl = "learn/real-time-streaming-big-data/home/welcome";
+    private static String courseName = "Big Data Applications: Real-Time Streaming";
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scrapper.getInstance().scrape(courseOverviewUrl, courseName);
     }
 }
--- src/main/java/bytes/sync/selenium/Scrapper.java
+++ src/main/java/bytes/sync/selenium/Scrapper.java
@@ -1,10 +1,21 @@
 package bytes.sync.selenium;
 
+import bytes.sync.downloader.DownloaderUtil;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 public class Scrapper {
 
-    public static Scrapper instance = new Scrapper();
+    private static final String CHROME_PROFILE_ARGUMENT = "user-data-dir=C:\\Users\\katakuri\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+    private static final String HOME_URL = "https://www.coursera.org/";
+
+    private static Scrapper instance = new Scrapper();
     private static ChromeDriver driver;
 
     private Scrapper() {
@@ -12,19 +23,38 @@ public class Scrapper {
     }
 
     private void initWebDriver() {
-        driver = new ChromeDriver();
+
+        //Using the driver manager to decouple web-driver with the program
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(CHROME_PROFILE_ARGUMENT);
+        driver = new ChromeDriver(options);
     }
 
     public static Scrapper getInstance() {
         return instance;
     }
 
-    public static void scrape(String courseOverviewUrl) {
+    public void scrape(String courseOverviewUrl, String courseName) {
 
-        driver.navigate().to(courseOverviewUrl);
+        driver.navigate().to(HOME_URL + courseOverviewUrl);
 
+        //This is the overview page, check for the number of weeks mentioned in the left navigation pane
+        WebElement weekCollectionDiv = driver.findElementByClassName("rc-NavigationDrawer");
+        List<WebElement> aTagsForWeeks = weekCollectionDiv.findElements(By.tagName("a"));
+        System.out.println("Number of weeks: "+aTagsForWeeks.size());
+
+        //Create Course Folder
+        DownloaderUtil.createFolder(courseName);
+
+        //For each week
+        int weekIndex = 1;
+        for(WebElement element : aTagsForWeeks) {
+            String weekUrl = HOME_URL + courseOverviewUrl.replace("welcome", "week/"+weekIndex);
+            driver.navigate().to(weekUrl);
+        }
 
     }
-
 
 }
